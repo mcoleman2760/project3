f5ce51076f788fd9bc18b3d9ab50e6e00ed3db70
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-20 02:01:31 -0500

    Update NearestNeighbor.hpp

--- NearestNeighbor.hpp
+++ NearestNeighbor.hpp
@@ -3,11 +3,12 @@
 #include <vector>
 #include <ctime>
 #include <algorithm>
+#include <list>
 #include <math.h>
 
 class Node {
 public:
-    int id;
+      int id;
     double x, y;
 
     Node(int node_id, double x_coord, double y_coord) : id(node_id), x(x_coord), y(y_coord) {}
@@ -15,10 +16,33 @@ public:
     double distance(const Node& other) const {
         return sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
     }
-
     // Equality operator
     bool operator==(const Node& other) const {
         return id == other.id && x == other.x && y == other.y;
+    }
+};
+class LinkedList {
+public:
+    std::list<Node> nodes;
+
+    void addNode(const Node& node) {
+        nodes.push_back(node);
+    }
+
+    void removeNode(const Node& node) {
+        nodes.remove(node);
+    }
+
+    bool empty() const {
+        return nodes.empty();
+    }
+
+    Node front() const {
+        return nodes.front();
+    }
+
+    void popFront() {
+        nodes.pop_front();
     }
 };
 
@@ -29,50 +53,45 @@ void nearestNeighbor(const std::string& filename) {
         return;
     }
 
-    std::vector<Node> nodes;
+    LinkedList unvisited;
     int node_id;
     double x, y;
 
     // Read node information from the file
     while (file >> node_id >> x >> y) {
-        nodes.emplace_back(node_id, x, y);
+        unvisited.addNode(Node(node_id, x, y));
     }
 
     file.close();
 
     clock_t start_time = clock();
 
-    // Initialize unvisited nodes with all nodes
-    std::vector<Node> unvisited(nodes);
-    Node current_node = nodes[0];
-    std::vector<Node> visited_nodes = {current_node};
+    Node current_node = unvisited.front();
+    LinkedList visited_nodes;
     double total_distance = 0;
 
     while (!unvisited.empty()) {
-        // Remove the current node from unvisited
-        unvisited.erase(std::remove(unvisited.begin(), unvisited.end(), current_node), unvisited.end());
+        unvisited.removeNode(current_node);
+        visited_nodes.addNode(current_node);
 
-        // Find the nearest neighbor to the current node
-        auto nearest_neighbor = std::min_element(unvisited.begin(), unvisited.end(),
+        auto nearest_neighbor = std::min_element(unvisited.nodes.begin(), unvisited.nodes.end(),
             [&current_node](const Node& a, const Node& b) {
                 return current_node.distance(a) < current_node.distance(b);
             }
         );
 
-        // Update total distance and move to the nearest neighbor
         total_distance += current_node.distance(*nearest_neighbor);
         current_node = *nearest_neighbor;
-        visited_nodes.push_back(current_node);
     }
 
     // Add the distance from the last node to the starting node
-    total_distance += visited_nodes.back().distance(visited_nodes[0]);
+    total_distance += visited_nodes.nodes.back().distance(visited_nodes.nodes.front());
 
     clock_t end_time = clock();
     double execution_time = double(end_time - start_time) / CLOCKS_PER_SEC * 1000;
 
     // Print the results
-    for (const auto& node : visited_nodes) {
+    for (const auto& node : visited_nodes.nodes) {
         std::cout << node.id << " ";
     }
 
