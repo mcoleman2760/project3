0fcaa00953a2aebce41c470fa7fdca61f6833d2e
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-12-21 02:12:55 -0500

    Update NearestNeighbor.hpp

--- NearestNeighbor.hpp
+++ NearestNeighbor.hpp
@@ -8,7 +8,7 @@
 
 class Node {
 public:
-      int id;
+    int id;
     double x, y;
 
     Node(int node_id, double x_coord, double y_coord) : id(node_id), x(x_coord), y(y_coord) {}
@@ -16,6 +16,7 @@ public:
     double distance(const Node& other) const {
         return sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
     }
+
     // Equality operator
     bool operator==(const Node& other) const {
         return id == other.id && x == other.x && y == other.y;
@@ -47,7 +48,7 @@ public:
     }
 };
 
-void nearestNeighbor(const std::string& filename) {
+void nearestNeighbor(std::string filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
@@ -56,20 +57,21 @@ void nearestNeighbor(const std::string& filename) {
 
     LinkedList unvisited;
     int node_id;
-    double x, y;
+    int x, y;
 
     // Read node information from the file
-    while (file >> node_id >> x >> y) {
+    while (file >> node_id ) {
         unvisited.addNode(Node(node_id, x, y));
     }
 
-    file.close();
+   file.close();
 
     clock_t start_time = clock();
 
-    Node current_node = unvisited.front();
+    Node start_node = unvisited.front();
+    Node current_node = start_node;
     LinkedList visited_nodes;
-    double total_distance = 0;
+    int total_distance = 0;
 
     while (!unvisited.empty()) {
         unvisited.removeNode(current_node);
@@ -81,18 +83,18 @@ void nearestNeighbor(const std::string& filename) {
             }
         );
 
-        total_distance += current_node.distance(*nearest_neighbor);
+        total_distance = current_node.distance(*nearest_neighbor) + total_distance;
         current_node = *nearest_neighbor;
     }
 
     // Add the distance from the last node to the starting node
-    total_distance += visited_nodes.nodes.back().distance(visited_nodes.nodes.front());
+    total_distance += current_node.distance(start_node);
 
     // Ensure the first node is visited in the end
-    visited_nodes.addNode(visited_nodes.nodes.front());
+    visited_nodes.addNode(start_node);
 
     clock_t end_time = clock();
-    double execution_time = double(end_time - start_time) / CLOCKS_PER_SEC * 1000;
+    int execution_time = double(end_time - start_time) / CLOCKS_PER_SEC * 1000;
 
     // Print the results
     for (const auto& node : visited_nodes.nodes) {
